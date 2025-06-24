9fc297c991075f449c2a71fad32c9418b204b5b7
manu <victorx811@gmail.com>
2025-06-24 15:33:55 -0300

    finished hero

---
+++ app/_components/NavigationBar.tsx
@@ -0,0 +1,251 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import {
+  HouseIcon,
+  Lightbulb,
+  MenuIcon,
+  ScrollText,
+  UsersRound,
+  Zap,
+} from "lucide-react";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const aboutTab = [
+  {
+    title: "Início",
+    href: "/docs/primitives/alert-dialog",
+    icon: HouseIcon,
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+
+    textColor: "text-neon",
+    bgColor: "bg-limao-escuro",
+    pipeColor: "bg-limao",
+  },
+  {
+    title: "Nosso time",
+    href: "/docs/primitives/scroll-area",
+    icon: UsersRound,
+    description: "Visually or semantically separates content.",
+
+    textColor: "text-lavanda-escuro",
+    bgColor: "bg-lavanda",
+    pipeColor: "bg-limao",
+  },
+  {
+    title: "Missão, visão e valores",
+    href: "/docs/primitives/hover-card",
+    icon: ScrollText,
+    description:
+      "For sighted users to preview content available behind a link.",
+
+    textColor: "text-neon",
+    bgColor: "bg-verde-musgo",
+    pipeColor: "bg-lavanda",
+  },
+  {
+    title: "O que nos move",
+    href: "/docs/primitives/tabs",
+    icon: Zap,
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+    textColor: "text-limao-escuro",
+    bgColor: "bg-limao",
+    pipeColor: "bg-lavanda",
+  },
+  {
+    title: "Nossa história",
+    href: "/docs/primitives/progress",
+    icon: Lightbulb,
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+
+    textColor: "text-lavanda",
+    bgColor: "bg-lavanda-escuro",
+    pipeColor: "bg-limao",
+  },
+];
+
+export function NavigationBar() {
+  return (
+    <nav className="fixed border-b border-verde-musgo h-[80px] w-full flex items-center justify-center z-50">
+      <div className="flex w-4/5 items-center justify-between">
+        <div className="flex flex-row gap-12">
+          <Image
+            alt="Logomarca da navbar"
+            src={"/svg/nav-logo.svg"}
+            width={96}
+            height={47}
+          />
+          <NavigationMenu viewport={false}>
+            <NavigationMenuList>
+              <NavigationMenuLink>Início</NavigationMenuLink>
+
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>
+                  Produtos e serviços
+                </NavigationMenuTrigger>
+                <NavigationMenuContent className="flex flex-row">
+                  <NavigationMenuLink asChild>
+                    <Image
+                      alt="Captura de tela do app"
+                      src={"/images/navbar/app_print.png"}
+                      width={250}
+                      height={340}
+                    />
+                  </NavigationMenuLink>
+
+                  <ul className="flex flex-col items-center justify-evenly w-[400px] xl:w-[500px]">
+                    <div className="flex flex-col gap-1 w-2/3">
+                      <div className="flex flex-row gap-2 relative">
+                        <p className="text-sm leading-none font-medium z-10">
+                          ISA Tech
+                        </p>
+
+                        <div className="w-1/4 bg-lavanda h-1 left-0 absolute bottom-0 z-0 hidden lg:flex" />
+                      </div>
+
+                      <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has
+                      </p>
+                    </div>
+
+                    <div className="flex flex-col gap-1 w-2/3">
+                      <div className="flex flex-row gap-2 relative">
+                        <p className="text-sm leading-none font-medium z-10">
+                          ISA Clinic
+                        </p>
+
+                        <div className="w-1/4 bg-limao h-1 left-0 absolute bottom-0 z-0 hidden lg:flex" />
+                      </div>
+
+                      <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has
+                      </p>
+                    </div>
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>Sobre nós</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid gap-2 w-[500px] md:grid-cols-2">
+                    {aboutTab.map((item) => (
+                      <ListItem
+                        key={item.title}
+                        title={item.title}
+                        href={item.href}
+                        icon={item.icon}
+                        textColor={item.textColor}
+                        bgColor={item.bgColor}
+                        pipeColor={item.pipeColor}
+                      >
+                        {item.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+
+              <NavigationMenuLink>Valores</NavigationMenuLink>
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+
+        <Button size={"sm"}>Acessar plataforma</Button>
+      </div>
+
+      <ul className="flex flex-row w-4/5 h-full items-center justify-between lg:hidden">
+        <Image
+          alt="Logomarca da navbar"
+          src={"/svg/nav-logo.svg"}
+          width={96}
+          height={47}
+        />
+
+        <button className="p-4">
+          <MenuIcon className="text-verde-musgo" />
+        </button>
+      </ul>
+    </nav>
+  );
+}
+
+interface ListItemProps {
+  title: string;
+  children: React.ReactNode;
+  href: string;
+  icon?: React.ElementType;
+  textColor?: string;
+  bgColor?: string;
+  pipeColor?: string;
+}
+
+function ListItem({
+  title,
+  children,
+  href,
+  icon,
+  textColor = "",
+  bgColor = "",
+  pipeColor = "",
+  ...props
+}: ListItemProps) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="flex gap-4">
+            <div
+              className={cn(
+                "flex rounded-full items-center justify-center w-10 h-10",
+                bgColor
+              )}
+            >
+              {icon ? (
+                React.createElement(icon, {
+                  className: textColor,
+                })
+              ) : (
+                <HouseIcon className={textColor} />
+              )}
+            </div>
+
+            <div className="flex flex-col gap-1 w-2/3">
+              <div className="flex flex-row gap-2 relative">
+                <p className="text-sm leading-none font-medium z-10">{title}</p>
+
+                <div
+                  className={cn(
+                    "w-1/4 bg-lavanda h-1 left-0 absolute bottom-0 z-0 hidden lg:flex",
+                    pipeColor
+                  )}
+                />
+              </div>
+
+              <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+                {children}
+              </p>
+            </div>
+          </div>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "@/components/ui/link";
 import Image from "next/image";
-import { MenuIcon } from "lucide-react";
+import HeroImage from "./_components/HeroImage.";
+import { NavigationBar } from "./_components/NavigationBar";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-screen bg-white">
-      <nav className="fixed border-b border-verde-musgo h-[80px] w-full flex items-center justify-center">
-        <ul className="flex flex-row w-4/5 h-full items-center justify-between">
-          <Image
-            alt="Logomarca da navbar"
-            src={"/svg/nav-logo.svg"}
-            width={96}
-            height={47}
-          />
-
-          <button className="p-4">
-            <MenuIcon className="text-verde-musgo" />
-          </button>
-        </ul>
-      </nav>
+      <NavigationBar />
 
       <section
         id="hero"
-        className="h-full min-h-fit items-center flex-col justify-center flex w-full pt-[120px]"
+        className="h-full min-h-fit items-center flex-col justify-center flex w-full pt-[120px] lg:pt-[10%] lg:flex-row lg:justify-evenly"
       >
-        <Image
-          alt="Imagem da hero mobile"
-          src="/images/hero/mobile.png"
-          width={320}
-          height={280}
-          className="floating z-20"
-        />
-        <div className="hero_bg w-full h-[280px] absolute top-[120px] left-0 z-0" />
+        <HeroImage />
 
-        <div className="flex justify-center w-4/5 flex-col text-verde-musgo text-center items-center gap-6">
-          <h1 className="font-bold text-2xl lg:text-3xl">
-            Simplifique sua rotina com uma plataforma completa
-          </h1>
+        <div className="hero_bg w-full h-[280px] absolute top-[120px] left-0 z-0 lg:h-full lg:top-0" />
+
+        <div className="flex justify-center w-4/5 lg:w-1/3 flex-col text-verde-musgo text-center items-center gap-6 lg:items-start lg:text-start z-10">
+          <div className="flex relative">
+            <h1 className="text-2xl lg:text-5xl z-10">
+              Simplifique sua rotina com uma plataforma completa
+            </h1>
+            <div className="w-1/2 bg-lavanda h-2 absolute bottom-1 z-0 hidden lg:flex" />
+          </div>
 
           <h2 className="lg:text-lg">
             A revolução digital para clínicas e médicos está aqui
           </h2>
 
-          <div className="flex w-full justify-between gap-2">
-            <a
+          <div className="flex w-full justify-between gap-2 lg:w-fit lg:gap-4">
+            <Link
               href="#"
               className="rounded-xl font-semibold bg-verde-musgo text-neon px-5 lg:px-8 py-4 items-center justify-center flex text-base lg:text-lg"
             >
               Saber mais
-            </a>
-            <button className="rounded-xl font-semibold border border-verde-musgo text-verde-musgo px-5 lg:px-8 py-4 items-center justify-center flex text-base lg:text-lg">
-              Acessar plataforma
-            </button>
+            </Link>
+            <Button variant={"outline"}>Acessar plataforma</Button>
           </div>
 
           <Image
@@ -62,7 +48,7 @@ export default function Home() {
 
           <p>Somos mais de 30.000 mil</p>
 
-          <div className="flex flex-row items-center w-full justify-evenly">
+          <div className="flex flex-row items-center w-full lg:w-fit lg:gap-4 justify-evenly">
             <a href="#" className="underline flex flex-row items-center gap-3">
               <Image
                 alt="Clientes"
