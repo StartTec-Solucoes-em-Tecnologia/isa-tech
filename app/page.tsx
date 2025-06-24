d38c394a2859143493b3036a124c15b926e609a7
manu <victorx811@gmail.com>
2025-06-24 09:11:22 -0300

    Hero mobile finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,90 @@
 import Image from "next/image";
+import { MenuIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="min-h-screen w-screen bg-white">
+      <nav className="fixed border-b border-verde-musgo h-[80px] w-full flex items-center justify-center">
+        <ul className="flex flex-row w-4/5 h-full items-center justify-between">
+          <Image
+            alt="Logomarca da navbar"
+            src={"/svg/nav-logo.svg"}
+            width={96}
+            height={47}
+          />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <button className="p-4">
+            <MenuIcon className="text-verde-musgo" />
+          </button>
+        </ul>
+      </nav>
+
+      <section
+        id="hero"
+        className="h-full min-h-fit items-center flex-col justify-center flex w-full pt-[120px]"
+      >
+        <Image
+          alt="Imagem da hero mobile"
+          src="/images/hero/mobile.png"
+          width={320}
+          height={280}
+          className="floating z-20"
+        />
+        <div className="hero_bg w-full h-[280px] absolute top-[120px] left-0 z-0" />
+
+        <div className="flex justify-center w-4/5 flex-col text-verde-musgo text-center items-center gap-6">
+          <h1 className="font-bold text-2xl lg:text-3xl">
+            Simplifique sua rotina com uma plataforma completa
+          </h1>
+
+          <h2 className="lg:text-lg">
+            A revolução digital para clínicas e médicos está aqui
+          </h2>
+
+          <div className="flex w-full justify-between gap-2">
+            <a
+              href="#"
+              className="rounded-xl font-semibold bg-verde-musgo text-neon px-5 lg:px-8 py-4 items-center justify-center flex text-base lg:text-lg"
+            >
+              Saber mais
+            </a>
+            <button className="rounded-xl font-semibold border border-verde-musgo text-verde-musgo px-5 lg:px-8 py-4 items-center justify-center flex text-base lg:text-lg">
+              Acessar plataforma
+            </button>
+          </div>
+
+          <Image
+            alt="Clientes"
+            src={"/images/hero/users.png"}
+            width={98}
+            height={34}
+          />
+
+          <p>Somos mais de 30.000 mil</p>
+
+          <div className="flex flex-row items-center w-full justify-evenly">
+            <a href="#" className="underline flex flex-row items-center gap-3">
+              <Image
+                alt="Clientes"
+                src={"/svg/apple-store.svg"}
+                width={24}
+                height={24}
+              />
+              <p>Apple Store</p>
+            </a>
+
+            <a href="#" className="underline flex flex-row items-center gap-3">
+              <Image
+                alt="Clientes"
+                src={"/svg/play-store.svg"}
+                width={24}
+                height={24}
+              />
+              <p>Play Store</p>
+            </a>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </main>
   );
 }
