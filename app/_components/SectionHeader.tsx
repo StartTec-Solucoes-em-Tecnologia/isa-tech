d907347b33c9bad7b523d4dd8edec15363a470ad
manu <victorx811@gmail.com>
2025-07-06 13:28:51 -0300

    hero updated to new design

--- app/_components/SectionHeader.tsx
+++ app/_components/SectionHeader.tsx
@@ -1,5 +1,3 @@
-import { cn } from "@/lib/utils";
-
 interface SectionHeaderProps {
   title: string;
   pipeColor: "bg-lavanda" | "bg-limao";
@@ -7,20 +5,20 @@ interface SectionHeaderProps {
 
 export default function SectionHeader({
   title,
-  pipeColor,
-}: SectionHeaderProps) {
+}: // pipeColor,
+SectionHeaderProps) {
   return (
     <div className="flex items-center justify-center relative">
-      <h2 className="font-bold text-xl z-20 xl:text-3xl 2xl:text-4xl text-gray-700 text-center">
+      <h2 className="text-xl z-20 xl:text-3xl 2xl:text-4xl text-gray-700 text-center">
         {title}
       </h2>
 
-      <div
+      {/* <div
         className={cn(
           "flex absolute w-2/3 right-0 bottom-1 z-10 h-1",
           pipeColor
         )}
-      />
+      /> */}
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,17 @@ import SectionHeader from "./_components/SectionHeader";
 import FadeCarrossel from "./_components/FadeCarrossel";
 import FeedbackCard from "./_components/FeedbackCard";
 import Motion from "./_components/Motion";
+import { NumberTicker } from "@/components/magicui/number-ticker";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+
+const words = `Transforme sua rotina médica com mais leveza, inteligência e segurança.`;
 
 export default function Home() {
   return (
     <main className="h-full w-full bg-white">
       <NavigationBar />
 
-      <div className="hero_bg w-full h-[280px] absolute top-[120px] left-0 z-0 lg:h-full lg:top-0" />
+      <div className="bg-limao/30 w-full h-[280px] absolute top-[120px] left-0 z-0 lg:h-full lg:top-0" />
       <div className="hero_gradient w-full h-[280px] absolute top-[120px] left-0 z-0 lg:h-full lg:top-0" />
 
       <motion.div
@@ -27,28 +31,75 @@ export default function Home() {
       >
         <section
           id="hero"
-          className="h-full min-h-[80vh] items-center flex-col justify-center flex w-full pt-[120px] lg:pt-[10%] lg:flex-row lg:justify-evenly z-20"
+          className="h-full min-h-[80vh] items-center flex-col justify-center flex w-full pt-[120px] sm:pt-[160px] lg:pt-[80px] lg:flex-row lg:justify-evenly z-20"
         >
-          <Image
-            alt="Imagem da hero"
-            src="/images/hero/desktop.png"
-            width={700}
-            height={610}
-            // sizes="(max-width: 768px) 320px, (max-width: 1536px) 533px, 700px"
-            className="floating z-20 w-[320px] h-[280px] sm:w-[400px] sm:h-[350px] xl:w-[533px] xl:h-[465px] 2xl:w-[700px] 2xl:h-[610px]"
-            priority
-          />
+          <div className="flex relative w-full lg:w-1/3 lg:ml-[5%] lg:xl:ml-[8%] justify-center lg:justify-start">
+            <Image
+              alt="Imagem da hero"
+              src="/images/hero/medic.png"
+              width={800}
+              height={800}
+              // sizes="(max-width: 768px) 320px, (max-width: 1536px) 533px, 700px"
+              className="z-20 w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] xl:w-[350px] xl:h-[350px] 2xl:w-[450px] 2xl:h-[450px] object-contain"
+              priority
+            />
+            <Image
+              alt="Imagem da hero"
+              src="/images/hero/chat.png"
+              width={400}
+              height={400}
+              // sizes="(max-width: 768px) 320px, (max-width: 1536px) 533px, 700px"
+              className="floating-bottom z-20 w-[120px] h-[120px] sm:w-[94px] sm:h-[132px] 2xl:w-[129px] 2xl:h-[181px] object-contain absolute -top-[12%] left-[8%] sm:left-[18%] md:left-[25%] lg:-top-[12%] lg:-left-[8%]"
+              priority
+            />
+            <Image
+              alt="Imagem da hero"
+              src="/images/hero/dra.png"
+              width={200}
+              height={200}
+              // sizes="(max-width: 768px) 320px, (max-width: 1536px) 533px, 700px"
+              className="floating-top z-20 w-[60px] h-[60px] sm:w-[84px] sm:h-[84px] 2xl:w-[129px] 2xl:h-[129px] object-contain absolute bottom-[10%] left-[3%] sm:bottom-[25%] sm:left-[7%] lg:bottom-[34%] lg:-left-[28%]"
+              priority
+            />
 
-          <div className="flex justify-center w-4/5 lg:w-[40%] 2xl:w-1/3 flex-col text-verde-musgo text-center items-center gap-4 sm:gap-6 sm:pt-6 2xl:gap-6 lg:items-start lg:text-start z-20">
-            <div className="flex relative">
-              <h1 className="text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl z-10 lg:font-semibold">
-                A revolução digital para clínicas e médicos está aqui
-              </h1>
-              <div className="w-[60%] bg-lavanda h-2 xl:h-3 absolute bottom-1 xl:bottom-0 z-0 hidden lg:flex" />
-            </div>
+            <Image
+              alt="Imagem da hero"
+              src="/images/hero/graphic.png"
+              width={600}
+              height={600}
+              // sizes="(max-width: 768px) 320px, (max-width: 1536px) 533px, 700px"
+              className="floating-bottom z-20 w-[140px] h-[140px] sm:w-[194px] sm:h-[125px] 2xl:w-[266px] 2xl:h-[172px] object-contain absolute -bottom-[40%] left-[20%] sm:-bottom-[20%] lg:-bottom-[20%] lg:-left-[18%]"
+              priority
+            />
 
-            <h2 className="2xl:text-lg">
-              Simplifique sua rotina com uma plataforma completa
+            <Image
+              alt="Imagem da hero"
+              src="/images/hero/patient.png"
+              width={200}
+              height={200}
+              // sizes="(max-width: 768px) 320px, (max-width: 1536px) 533px, 700px"
+              className="floating-top z-20 w-[50px] h-[50px] sm:w-[60px] sm:h-[60px] 2xl:w-[100px] 2xl:h-[100px] object-contain absolute -bottom-[30%] right-[25%] lg:-bottom-[32%] lg:right-[38%]"
+              priority
+            />
+
+            <Image
+              alt="Imagem da hero"
+              src="/images/hero/dashboard.png"
+              width={600}
+              height={600}
+              // sizes="(max-width: 768px) 320px, (max-width: 1536px) 533px, 700px"
+              className="floating-bottom z-20 w-[125px] h-[125px] sm:w-[242px] sm:h-[125px] 2xl:w-[277px] 2xl:h-[179px] object-contain absolute bottom-[5%] right-[4%] lg:-bottom-[6%] lg:-right-[12%] 2xl:-right-[5%]"
+              priority
+            />
+          </div>
+
+          <div className="flex mt-20 lg:mt-16 justify-center w-4/5 lg:w-[40%] flex-col text-verde-musgo text-center items-center gap-4 sm:gap-6 sm:pt-6 2xl:gap-6 lg:items-start lg:text-start z-20">
+            <TextGenerateEffect duration={2} filter={false} words={words} />
+
+            <h2 className="text-sm 2xl:text-lg">
+              A ISA TECH é o sistema de gestão completo que simplifica
+              processos, protege dados e devolve tempo para o que realmente
+              importa: cuidar de vidas.
             </h2>
 
             <div className="flex w-full justify-between gap-2 sm:w-fit lg:gap-4">
@@ -60,13 +111,21 @@ export default function Home() {
             <Image
               alt="Usuários da plataforma"
               src="/images/hero/users.png"
-              width={98}
-              height={34}
+              width={200}
+              height={400}
               sizes="(max-width: 768px) 80px, 98px"
               className="w-[80px] h-[28px] lg:w-[98px] lg:h-[34px]"
             />
 
-            <p>Somos mais de 30.000 mil</p>
+            <p>
+              Somos mais de{" "}
+              <NumberTicker
+                value={30.0}
+                decimalPlaces={3}
+                className="whitespace-pre-wrap text-verde-musgo"
+              />{" "}
+              mil
+            </p>
 
             <div className="flex flex-row items-center w-full sm:w-fit sm:gap-4 justify-evenly">
               <a
